56e7f9f77f0adeb5820b6f940db6b3f8df208698
Amit Biswas <biswas1992@hotmail.com>
2019-06-05 17:29:05 -0400

    Initial Commit

---
+++ Taskflyer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taskflyer
+//
+//  Created by Maze Geek on 6/5/19.
+//  Copyright © 2019 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
