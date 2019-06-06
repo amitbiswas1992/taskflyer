baa86f82f78ea8efc003ef799c804449cf547178
Amit Biswas <biswas1992@hotmail.com>
2019-06-06 04:34:19 -0400

    Added table view for the task

---
+++ TodoListTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TodoListTableViewController.swift
+//  Taskflyer
+//
+//  Created by Maze Geek on 6/6/19.
+//  Copyright © 2019 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class TodoListTableViewController: UITableViewController {
+let itemArray = ["Find Mike", "Buy Eggs", "TLC Papers Works" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+
+        
+    }
+ 
+    //MARK: Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+    
+}
